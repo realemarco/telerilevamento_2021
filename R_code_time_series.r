eb4932e8e44864d82a0fd5c48b11b53bebb10bc7
realemarco <80755012+realemarco@users.noreply.github.com>
2021-03-31 13:01:12 +0200

    Create R_code_time_series.r

---
+++ R_code_time_series.r
@@ -0,0 +1,7 @@
+#time series analysis
+#Greenland increase of temperature
+#data and code from Emanuela Cosma
+
+library(raster)
+setwd("C:/lab/greenland")
+#dato che i dati sono nella cartella greenland, settando la wd dobbiamo mettere il nome di questa cartella
