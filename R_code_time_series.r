89cfcc1e6f2a35e91c80c4ad794b2b7ed0b7010c
realemarco <80755012+realemarco@users.noreply.github.com>
2021-04-19 17:24:34 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -91,4 +91,95 @@ plotRGB(TGr, 2, 3, 4, stretch="Lin")
 #per finire installiamo il pacchetto rasterVis
 #install.packages("rasterVis")
 
+###LEZIONE DEL 09/04/21
+
+#rasterVis: per visualizzazioni di file raster
+
+library(raster)
+library(rasterVis)
+
+setwd("C:/lab/greenland")
+    
+rlist <- list.files(pattern="lst")
+rlist
+
+
+import <- lapply(rlist,raster)
+import
+
+
+TGr <- stack(import)
+TGr
+
+#abbiamo ricaricato i file della volta scorsa
+
+#facciamo ora un levelplot
+
+levelplot(TGr)
+levelplot(TGr$lst_2000)
+#levelplot(TGr$lst_2000) serve per visualizzare un singolo livello
+#per ogni pixel abbiamo un valore di temperatura
+#per ogni colonna viene fatta una media dei valori e queste medie vengono riportate nel grafico sopra l'immagine
+
+#cambiamo i colori usando la colorRampPalette
+levelplot(TGr)
+cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
+
+#l'argomento su levelplot per cambiare colore non è col ma col.regions
+levelplot(TGr, col.regions=cl)
+
+#la differenza tra levelplot e la semplice funzione plot:
+#con plot si ha per ogni immagine una legenda (che ruba spazio) e generalmente è meno ordinato di levelplot
+
+#possiamo agire sui nomi delle singole mappe (titoli)
+#i singoli strati di uno stack si chiamano attributi
+#abbiamo quattro attributi (lst_2000, lst_2005, lst_2010, lst_2015)
+
+#possiamo cambiare i nomi degli attributi usando l'argomento "names.attr" della funzione levelplot
+levelplot(TGr,col.regions=cl,main="Summer land surface temperature", names.attr=c("July 2000","July 2005","July 2010","July 2015"))
+#siccome sono quattro blocchi (vettore di quattro cose diverse) devo mettere la "c" davanti
+#con l'argomento "main" inseriamo il titolo
+
+
+
+#melt
+#dati sulla copertura di ghiaccio da satellite usando le microonde (non assorbite dal ghiaccio)
+
+#carichiamo i file della cartella melt usando list.files 
+
+meltlist<-list.files(pattern="melt")
+meltlist
+
+#usiamoora lapply alla lista appena creata e applichiamogli la funzione raster
+meltimport<-lapply(meltlist, raster)
+meltimport
+
+#ora raggruppiamo i file con la funzione stack
+melt<-stack(meltimport)
+melt
+#plottiamo con levelplot
+levelplot(melt)
+#vediamo i valori dello scoglimento dei ghiacci
+#più alto è il valore maggiore sarà lo scioglimento
+
+#abbiamo una matrice di dati (una matrice di pixel) con dei valori che rappresentano il tasso di scioglimento
+#nei diversi anni per lo stesso pixel ho valori diversi 
+#allora facciamo la sottrazione dei valori per gli stessi pixel su matrici diverse (sui file dei diversi anni)
+#2007 meno 1979 ci da la differenza
+
+#facciamo la differenza tra le due immagini
+melt_amount <- melt$X2007annual_melt - melt$X1979annual_melt
+#mettiamo $ per identificare il file che ci interessa in mezzo allo stack
+#plottiamo ora i file e modifichiamo i colori
+
+clb <- colorRampPalette(c("blue","white","red"))(100)
+plot(melt_amount, col=clb)
+#in questo grafico, tutte le zone rosse sono quelle in cui dal 2007 al 1979 (i due estremi temporali)
+#c'è stato uno scioglimento del ghiaccio maggiore
+#perchè abbiamo soltato il valore dello scioglimento del 2007 a quello del 1979
+
+#facciamo ora un levelplot di melt_amount
+levelplot(melt_amount, col.regions=clb)
+
+#per prossima volta installare pacchetto knitr
 
