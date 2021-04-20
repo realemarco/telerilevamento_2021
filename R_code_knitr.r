29b477072b0451a20c807bcfe222a02ec63fbc83
realemarco <80755012+realemarco@users.noreply.github.com>
2021-04-20 09:35:31 +0200

    Create R_code_knitr.r

---
+++ R_code_knitr.r
@@ -0,0 +1,10 @@
+#R_code_knitr
+
+#install.packages("knitr")
+
+setwd("C:/lab/")
+library(knitr)
+
+#con knitr r può usare un codice esterno, importarlo in r e generare un report
+#questo report verrà salvato dentro alla cartella dove abbiamo preso il codice precedente
+
