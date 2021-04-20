b1aafbe2bd41be65412f0139c6322c2aca1bebdc
realemarco <80755012+realemarco@users.noreply.github.com>
2021-04-20 10:01:09 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -1,3 +1,5 @@
+#LEZIONE 16/04/21
+
 #R_code_knitr
 
 #install.packages("knitr")
@@ -7,4 +9,17 @@ library(knitr)
 
 #con knitr r può usare un codice esterno, importarlo in r e generare un report
 #questo report verrà salvato dentro alla cartella dove abbiamo preso il codice precedente
+
+#prendiamo tutto il codice di una precedente lezione e lo incolliamo in un qualsiasi gestore di testo
+#salviamo il file nella cartella C:/lab/
+#IMPORTANTE: non dev'esserci dev.off() nel codice del file di testo
+
+#andiamo ad usare la funzione "stitch"
+#come primo argomento da dare alla funzione è come si chiama il codice che abbiamo appena salvato
+#in questo caso è "r_code_greenland.r"
+
+#altro argomento è il temeplate (in questo caso "misc")
+#poi va messo il nome del pacchetto da utilizzare ("knitr")
+
+stitch("C:/lab/r_code_greenland.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
 
