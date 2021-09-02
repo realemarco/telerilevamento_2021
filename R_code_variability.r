9e48f79a2ad759db2540de3fc83f4d1c873d6c9c
realemarco <80755012+realemarco@users.noreply.github.com>
2021-09-02 12:35:16 +0200

    Create R_code_variability.r

---
+++ R_code_variability.r
@@ -0,0 +1,8 @@
+setwd("C:/lab/")
+library(raster)
+library(RStoolbox)
+library(ggplot2) #for ggplot plotting
+library(gridExtra) #for plotting ggplots togheter
+
+#install.packages("viridis") #pacchetto con scale di colore predefinite
+library (viridis)
