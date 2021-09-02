662c023e77f2a6970fd4cc2f64384bd66d6a28b5
realemarco <80755012+realemarco@users.noreply.github.com>
2021-09-02 11:57:23 +0200

    Create R_code_land_cover.r

--- R_code_land_cover.r
+++ R_code_land_cover.r
@@ -42,3 +42,43 @@ grid.arrange(p1, p2, nrow=2)
 #la funzione grid.arrange compone come vogliamo il multiframe
 #diamo un nome al primo plot, uno al secondo e grid.arrange li mette insieme; poi Nrow = n di righe. 
 #il plot risultante è più informativo del multiframe con "par"
+
+#CONTINUO LAND COVER
+
+setwd("C:/lab/")
+library(raster)
+library(RStoolbox) 
+library(ggplot2)
+library(gridExtra)
+
+#importiamo le immagini dell'Amazzonia 
+defor1 <- brick("defor1.jpg")
+defor2 <- brick("defor2.jpg")
+
+#vediamo ora l'indice vegetazionale suddiviso in due classi
+d1c <- unsuperClass(defor1, nClasses=2)
+plot(d1c$map)
+
+#eseguiamo la stessa cosa ma con defor2 (prima con 2 poi con 3 classi)
+d2c <- unsuperClass(defor2, nClasses=2)
+plot(d2c$map)
+d2c3 <- unsuperClass(defor2, nClasses=3)
+plot(d2c3$map)
+
+
+#calcoliamo adesso la frequenza fra la prima e la seconda classe per le due immagini 
+freq(d1c$map)  # per s1 la dimensione è: s1=304725
+#in questo caso la frequenza totale di foresta è maggiore rispetto al resto (se si somano i due valori si raggiungono le dimensioni totali dell'immagine)
+
+#facciamo lo stesso per defor2
+freq(d2c$map) # per s2 la dimensione è: s2=164908
+
+#adesso eseguiamo un calcolo
+s1<-304725
+prop1 <- freq(d1c$map) / s1
+prop1
+
+s2<-164908
+prop2<- freq(d2c$map) / s2
+prop2
+#così facendo abbiamo proporzionato i valori dimensionali, in modo da essere più accurati
