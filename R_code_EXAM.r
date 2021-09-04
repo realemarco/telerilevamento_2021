2e89bc49df1f184e7fd695de96ef468fa4530d2a
realemarco <80755012+realemarco@users.noreply.github.com>
2021-09-04 16:59:40 +0200

    Update R_code_EXAM.r

--- R_code_EXAM.r
+++ R_code_EXAM.r
@@ -4,4 +4,39 @@ library(RStoolbox)
 library(ggplot2)
 
 y1987 <- brick("Yellowstone_19870805_lrg.jpg")
-plot(y1987)
+y1989 <- brick("Yellowstone_19890802_lrg.jpg")
+y1994 <- brick("Yellowstone_19940925_lrg.jpg")
+y1999 <- brick("Yellowstone_19990923_lrg.jpg")
+y2004 <- brick("Yellowstone_20041006_lrg.jpg")
+y2009 <- brick("Yellowstone_20090902_lrg.jpg")
+y2014 <- brick("yellowstone_oli_2014291_lrg.jpg")
+y2019 <- brick ("yellowstone_oli_2019193_lrg.jpg")
+
+y1987
+y1989
+y1994
+y1999
+y2004
+y2009
+y2014
+y2019
+
+par(mfrow=c(2,4))
+plotRGB(y1987, r=1, g=2, b=3, stretch="lin")
+plotRGB(y1989, r=1, g=2, b=3, stretch="lin")
+plotRGB(y1994, r=1, g=2, b=3, stretch="lin")
+plotRGB(y1999, r=1, g=2, b=3, stretch="lin")
+plotRGB(y2004, r=1, g=2, b=3, stretch="lin")
+plotRGB(y2009, r=1, g=2, b=3, stretch="lin")
+plotRGB(y2014, r=1, g=2, b=3, stretch="lin")
+plotRGB(y2019, r=1, g=2, b=3, stretch="lin")
+
+
+#1 carico tutte le immagini
+#2 dati sulle immagini
+#3 plot delle immagini
+#4 mettere etichette immagini
+
+
+#time series
+#vegetation index
