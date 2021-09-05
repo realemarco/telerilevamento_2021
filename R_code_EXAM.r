abc6ab0ed255024e3da5761991e3c2bcb5a03f4e
realemarco <80755012+realemarco@users.noreply.github.com>
2021-09-05 16:24:24 +0200

    Update R_code_EXAM.r

--- R_code_EXAM.r
+++ R_code_EXAM.r
@@ -2,6 +2,7 @@ setwd("C:/lab/esame/")
 library(raster)
 library(RStoolbox)
 library(ggplot2)
+library(rasterVis)
 
 y1987 <- brick("Yellowstone_19870805_lrg.jpg")
 y1989 <- brick("Yellowstone_19890802_lrg.jpg")
@@ -30,6 +31,50 @@ plotRGB(y2004, r=1, g=2, b=3, stretch="lin")
 plotRGB(y2009, r=1, g=2, b=3, stretch="lin")
 plotRGB(y2014, r=1, g=2, b=3, stretch="lin")
 plotRGB(y2019, r=1, g=2, b=3, stretch="lin")
+
+dev.off()
+
+#UNSUPERCLASS
+usc1987 <- unsuperClass(y1987, nClasses=3)
+usc1989 <- unsuperClass(y1989, nClasses=3)
+usc1994 <- unsuperClass(y1994, nClasses=3)
+usc1999 <- unsuperClass(y1999, nClasses=3)
+usc2004 <- unsuperClass(y2004, nClasses=3)
+usc2009 <- unsuperClass(y2009, nClasses=3)
+usc2014 <- unsuperClass(y2014, nClasses=3)
+usc2019 <- unsuperClass(y2019, nClasses=3)
+
+par(mfrow=c(2,4))
+plot(usc1987$map)
+plot(usc1989$map)
+plot(usc1994$map)
+plot(usc1999$map)
+plot(usc2004$map)
+plot(usc2009$map)
+plot(usc2014$map)
+plot(usc2019$map)
+
+#TIME SERIES
+
+#####SEZIONE PROVA######
+setwd("C:/lab/esame/")
+library(rasterVis)
+library(raster)
+library(RStoolbox)
+rlist <- list.files (pattern="lrg")
+rlist
+import<-lapply(rlist,brick)
+TGr<-stack(import)
+TGr
+par(mfrow=c(2,4))
+levelplot(TGr)
+par(mfrow=c(2,4))
+plotRGB(TGr, r=1, g=2, b=3, stretch="lin")
+############################################
+
+
+
+
 
 
 #1 carico tutte le immagini
