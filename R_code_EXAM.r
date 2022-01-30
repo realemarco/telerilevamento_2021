6a228063c35c764f4f71c35f62e00c4c13025f3f
realemarco <80755012+realemarco@users.noreply.github.com>
2022-01-30 20:14:30 +0100

    Update R_code_EXAM.r

--- R_code_EXAM.r
+++ R_code_EXAM.r
@@ -45,21 +45,7 @@ plotRGB(y2014, r=1, g=2, b=3, stretch="hist")
 plotRGB(y2019, r=1, g=2, b=3, stretch="hist")
 
 
-
-#PROVA GGPLOT##
-#library(gridExtra)
-#p1<-ggRGB(y1987, r=1, g=2, b=3, stretch="hist")
-#p2<-ggRGB(y1989, r=1, g=2, b=3, stretch="hist")
-#p3<-ggRGB(y1994, r=1, g=2, b=3, stretch="hist")
-#p4<-ggRGB(y1999, r=1, g=2, b=3, stretch="hist")
-#p5<-ggRGB(y2004, r=1, g=2, b=3, stretch="hist")    #SEMBRA NON FUNZIONARE
-#p6<-ggRGB(y2009, r=1, g=2, b=3, stretch="hist")
-#p7<-ggRGB(y2014, r=1, g=2, b=3, stretch="hist")
-#p8<-ggRGB(y2019, r=1, g=2, b=3, stretch="hist")
-#grid.arrange(p1, p2, p3, p4, p5, p6, p7, p8, nrow=2)
-##############################################################
-
-#plot solo prima banda (verde?)
+#plot solo prima banda (GREEN)
 par(mfrow=c(2,4))
 plot(y1987$Yellowstone_19870805_lrg.1)
 plot(y1989$Yellowstone_19890802_lrg.1)
@@ -71,7 +57,7 @@ plot(y2014$Yellowstone_oli_2014291_lrg.1)
 plot(y2019$Yellowstone_oli_2019193_lrg.1)
 
 
-#plot solo seconda banda (nir??)
+#plot solo seconda banda (NIR)
 par(mfrow=c(2,4))
 plot(y1987$Yellowstone_19870805_lrg.2)
 plot(y1989$Yellowstone_19890802_lrg.2)
@@ -82,7 +68,7 @@ plot(y2009$Yellowstone_20090902_lrg.2)
 plot(y2014$Yellowstone_oli_2014291_lrg.2)
 plot(y2019$Yellowstone_oli_2019193_lrg.2)
 
-#plot solo terza banda (swir??)
+#plot solo terza banda (SWIR)
 par(mfrow=c(2,4))
 plot(y1987$Yellowstone_19870805_lrg.3)
 plot(y1989$Yellowstone_19890802_lrg.3)
@@ -389,59 +375,13 @@ gr_inc <- ggplot(percentages, aes(x=anno, y=area_incendiata, group = 1)) + geom_
 gr_veg <- ggplot(percentages, aes(x=anno, y=vegetazione, group = 1)) + geom_line(color = "green")
 grid.arrange(gr_inc, gr_veg, nrow = 2)
 
-#####SEZIONE PROVA######
-setwd("C:/lab/esame/")
-library(rasterVis)
-library(raster)
-library(RStoolbox)
+#grafico lineare vegetazione matura x altra vegetazione
+anno0 <- c("1987", "1989","1994","1999", "2004", "2014")
+vegetazione_matura <- c(54, 43, 35, 31, 32, 40)
+altra_vegetazione <- c(32, 31, 23, 29, 28, 25)
+vegxveg <- data.frame(anno0, vegetazione_matura, altra_vegetazione)
+vegxveg
 
-rlist <- list.files (pattern="lrg")
-rlist
-import<-lapply(rlist,raster)
-import
-
-importcl<-lapply(import, unsuperClass, nClasses=3)
-plot(importcl)
-TGr<-stack(import)
-TGr
-
-plot(TGr)
-
-UT<-unsuperClass(TGr,nClasses=3)
-
-par(mfrow=c(2,4))
-levelplot(TGr,pattern="lrg.2")  ###capire se il pattern funziona davvero per la seconda banda
-levelplot(TGr$Yellowstone_19870805_lrg)
-par(mfrow=c(2,4))
-plotRGB(TGr, r=1, g=2, b=3, stretch="hist")
-############################################
-
-
-
-
-
-
-#1 carico tutte le immagini
-#2 dati sulle immagini
-#3 plot delle immagini
-#4 mettere etichette immagini
-
-
-
-#PROVA LISTA TIME SERIES UNSUPERCLASS
-usc1987 <- unsuperClass(y1987$Yellowstone_19870805_lrg.1, nClasses=3)
-usc1989 <- unsuperClass(y1989$Yellowstone_19890802_lrg.1, nClasses=3)
-usc1994 <- unsuperClass(y1994$Yellowstone_19940925_lrg.1, nClasses=3)
-usc1999 <- unsuperClass(y1999$Yellowstone_19990923_lrg.1, nClasses=3)     
-usc2004 <- unsuperClass(y2004$Yellowstone_20041006_lrg.1, nClasses=3)     ##FORSE BANDA VERDE EVIDENZIA INCENDIO
-usc2009 <- unsuperClass(y2009$Yellowstone_20090902_lrg.1, nClasses=3)
-usc2014 <- unsuperClass(y2014$Yellowstone_oli_2014291_lrg.1, nClasses=3)
-usc2019 <- unsuperClass(y2019$Yellowstone_oli_2019193_lrg.1, nClasses=3)
-
-lista<-c(usc1987,usc1989, usc1994, usc1999, usc2004, usc2009, usc2014, usc2019)
-lista
-plot(lista$map) ####HA FUSO TUTTE LE IMMAGINI IN UN'UNICA IMMAGINE
-levelplot(lista$map, col.regions=c1) #LO STESSO QUI, COME PLOTTARLE SEPARATAMENTE MA CON CLASSI COERENTI?
-
-#############################
-
+veg_mat <- ggplot(vegxveg, aes(x=anno0, y=vegetazione_matura, group = 1)) + geom_line(color = "dark green")
+gr_veg <- ggplot(vegxveg, aes(x=anno0, y=altra_vegetazione, group = 1)) + geom_line(color = " light green")
+grid.arrange(veg_mat, gr_veg, nrow = 2)
